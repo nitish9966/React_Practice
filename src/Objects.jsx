3d75ef0823aca13116c87fae1227ba6438eeb2ce
Nitish <nitishbabubolla9966@gmail.com>
2024-06-06 11:51:14 +0530

    Updated on 6-6-24

--- src/Objects.jsx
+++ src/Objects.jsx
@@ -4,9 +4,22 @@ function Objects(){
 
     const [object,setObject]=useState({year:2024,make:"Ford",model:"Mustang"})
 
+    function handleYear(event){
+        setObject({...object,year:event.target.value})
+    }
+    function handleMake(event){
+        setObject({...object,make:event.target.value})
+    }
+    function handleModel(event){
+        setObject({...object,model:event.target.value})
+    }
+
     return(
         <div>
             <p>Your Favorite Car is: {object.year} {object.make} {object.model}</p>
+            <input type='number' value={object.year} onChange={handleYear}/><br></br>
+            <input type='text' value={object.make} onChange={handleMake}/><br></br>
+            <input type='text' value={object.model} onChange={handleModel}/>
         </div>
     );
 }
